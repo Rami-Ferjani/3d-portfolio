b062012fbd5ef1f706eaeca694908dbb60ea34c0
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-04-24 22:54:41 +0100

    added the sliding words animation

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+];
 const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
@@ -11,9 +14,28 @@ const Hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
-                <h1>Shaping Designs</h1>
-                <h1> into Real Projects</h1>
-                <h1>that Deliver Results</h1>
+              <h1>
+                Shaping Designs
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1> into Real Projects</h1>
+              <h1>that Deliver Results</h1>
             </div>
           </div>
         </header>
