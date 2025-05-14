903470748d3d7ff41ee54ab3e03e9ef61f9b0f48
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-14 14:02:26 +0100

    added the first project in the showcase

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const ShowcaseSection = () => {
-    return (
-        <div>
-            ShowcaseSection
+  return (
+    <div id="work" className="app-showcase">
+      <div className="w-full">
+        <div className="showcaselayout">
+          {/* LEFT */}
+          <div className="first-project-wrapper">
+            <div className="image-wrapper">
+              <img src="/images/project1.png" alt="Ryde" />
+            </div>
+            <div className="text-content">
+              <h2>
+                On-Demand Rides Made Simple with a Powerfull,User-Friendly App
+                called Ryde
+              </h2>
+              <p className="text-white-50 md:text-xl">
+                An app built with React Native,Expo, and tailwindCSS for a
+                fast,user-friendly experience.
+              </p>
+            </div>
+          </div>
+
+          {/* Right */}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShowcaseSection;
