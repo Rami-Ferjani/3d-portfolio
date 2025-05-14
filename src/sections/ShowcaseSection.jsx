8bd74814ea5a4ce57c4e761f210a48cffa021b17
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-14 13:53:12 +0100

    added the showcase file

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowcaseSection = () => {
+    return (
+        <div>
+            ShowcaseSection
+        </div>
+    );
+};
+
+export default ShowcaseSection;
