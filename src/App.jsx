6a0f28fe917dfdda3a9a46ec1d767bd41b38dbac
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-15 17:24:49 +0100

    added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./sections/hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
     </>
