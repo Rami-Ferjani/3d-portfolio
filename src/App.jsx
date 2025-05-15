de74b477b1d4f762e600e346ea1049a0061184e7
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-15 18:48:25 +0100

    finished the features card

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Hero from "./sections/hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import LogoSection from "./components/LogoSection.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
 const App = () => {
   return (
     <>
-      <LogoSection />
+      {/* <LogoSection /> */}
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <FeatureCards />
     </>
   );
 };
