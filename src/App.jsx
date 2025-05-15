5681004ba428b577f13108859596df38b325326f
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-15 17:53:11 +0100

    added the logo section placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Hero from "./sections/hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
+import LogoSection from "./components/LogoSection.jsx";
 const App = () => {
   return (
     <>
+      <LogoSection />
       <NavBar />
       <Hero />
       <ShowcaseSection />
