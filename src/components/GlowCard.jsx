dd305e95ad3190eaa60a1617f61abef6fddaea4c
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-17 21:57:04 +0100

    added the glow card code

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -4,6 +4,15 @@ const GlowCard = ({ card, children }) => {
   return (
     <div className="card card-bordr timeline-card rounded-xl p-10">
       <div className="glow" />
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <img src="/images/star.png" key={i} alt="star" className="size-5" />
+        ))}
+      </div>
+      <div className="mb-5">
+        <p className="text-white-50 text-lg">{card.review}</p>
+      </div>
+      {children}
     </div>
   );
 };
