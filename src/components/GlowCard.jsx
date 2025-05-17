dc5ab68a8dd6fbb46b18abd5ff5dcf029f76a07d
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-17 22:05:06 +0100

    made the glowing card's border effect follow the cursor

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { useRef } from "react";
 
-const GlowCard = ({ card, children }) => {
+const GlowCard = ({ card, children, index }) => {
+  const cardRefs = useRef([]);
+  const handleMouseMove = (index) => (e) => {
+    const card = cardRefs.current[index];
+    if (!card) return;
+    //get the mouse position relative to card
+    const rect = card.getBoundingClinetRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clinetY - rect.top - rect.height / 2;
+
+    //Calc the angle from the center of the card
+    let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+    angle = (angle + 360) % 360;
+
+    card.style.setProperty("--start", angle + 60);
+  };
   return (
-    <div className="card card-bordr timeline-card rounded-xl p-10">
+    <div
+      ref={(el) => (cardRefs.current[index] = el)}
+      onMouseMove={handleMouseMove(index)}
+      className="card card-bordr timeline-card rounded-xl p-10"
+    >
       <div className="glow" />
       <div>
         {Array.from({ length: 5 }, (_, i) => (
