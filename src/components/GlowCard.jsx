ef54e5927654f84f95c16e81b94856395a26e087
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-19 17:30:24 +0100

    added the code for rigt side off the experience section

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -24,7 +24,7 @@ const GlowCard = ({ card, children, index }) => {
       className="card card-border timeline-card rounded-xl p-10"
     >
       <div className="glow" />
-      <div>
+      <div className="flex items-center gap-1 mb-5">
         {Array.from({ length: 5 }, (_, i) => (
           <img src="/images/star.png" key={i} alt="star" className="size-5" />
         ))}
