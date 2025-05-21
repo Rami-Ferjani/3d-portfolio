c2efe3fd39f5f032d25debad9658816b1ca2865a
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-21 23:03:03 +0100

    added the header for testomonials

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="testimonials" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="What People Say About me?"
+          sub="Client Feedback Highlights"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
