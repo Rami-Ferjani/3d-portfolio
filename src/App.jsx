7ff5c7608ea1f28284588b85f1ac655878574b8d
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-22 23:06:31 +0100

    added the contact section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FeatureCards from "./sections/FeatureCards.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
 import TechStack from "./sections/TechStack.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
+import ContactSection from "./sections/ContactSection.jsx";
 const App = () => {
   return (
     <>
@@ -18,6 +19,7 @@ const App = () => {
       <ExperienceSection />
       <TechStack />
       <Testimonials/>
+      <ContactSection />
     </>
   );
 };
