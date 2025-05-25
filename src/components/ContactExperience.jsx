e767587f137ed6b150d42a53d1af70097c233c27
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-05-25 22:37:58 +0100

    generated the form code using junie

---
+++ src/components/ContactExperience.jsx
@@ -0,0 +1,64 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+
+const ComputerModel = () => {
+  const computer = useGLTF("/models/computer-optimized-transformed.glb");
+  return (
+    <primitive 
+      object={computer.scene} 
+      scale={2.5}
+      position={[0, -1.5, 0]}
+      rotation={[0, -Math.PI / 4, 0]}
+    />
+  );
+};
+
+const ContactLights = () => {
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+      <spotLight
+        position={[0, 5, 5]}
+        intensity={80}
+        angle={0.15}
+        penumbra={0.4}
+        color="white"
+      />
+      <spotLight
+        position={[5, 2, 0]}
+        intensity={40}
+        angle={0.3}
+        penumbra={0.5}
+        color="#4cc9f0"
+      />
+      <pointLight position={[-5, 2, 0]} intensity={20} color="#7209b7" />
+    </>
+  );
+};
+
+const ContactExperience = () => {
+  return (
+    <div className="w-full h-full min-h-[400px]">
+      <Canvas camera={{ position: [0, 0, 8], fov: 45 }}>
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          maxDistance={10}
+          minDistance={5}
+          minPolarAngle={Math.PI / 5}
+          maxPolarAngle={Math.PI / 2}
+          autoRotate
+          autoRotateSpeed={0.5}
+        />
+        <ContactLights />
+        <Suspense fallback={null}>
+          <ComputerModel />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default ContactExperience;
