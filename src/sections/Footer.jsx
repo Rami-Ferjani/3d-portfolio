d1716b99d86d8fd1768256ebcbca7d533a152b51
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-06-30 18:39:03 +0100

    finished the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,15 +4,20 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-container">
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center md:items-start items-center">
           <a href="/">Visit my blog</a>
         </div>
         <div className="socials">
           {socialImgs.map((img) => (
-            <a>
+            <a className="icon" target="_blank" href={img.url} key={img.url}>
               <img src={img.imgPath} />
             </a>
           ))}
+        </div>
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            &#x2117; {new Date().getFullYear()} Rami | All rights reserved
+          </p>
         </div>
       </div>
     </div>
