a68c3586c1918d865dcb78d5132d2ec15669f5a0
Rami-Ferjani <rami.ferjani3@gmail.com>
2025-06-30 18:34:56 +0100

    added the socials images to the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { socialImgs } from "../constants/index";
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="flex flex-col justify-center">
+          <a href="/">Visit my blog</a>
+        </div>
+        <div className="socials">
+          {socialImgs.map((img) => (
+            <a>
+              <img src={img.imgPath} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
